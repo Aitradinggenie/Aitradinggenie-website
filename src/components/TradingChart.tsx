442b835782f0c59654148100663ec1e924914381
Hijo1918 <jonathanije3@gmail.com>
2025-08-15 23:22:34 +0100

    Create TradingChart.tsx

---
+++ src/components/TradingChart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TradingChart: React.FC = () => {
+  return (
+    <div className="h-96 bg-white/10 rounded-lg flex items-center justify-center">
+      <p className="text-white text-lg">Trading Chart Placeholder</p>
+      {/* Integrate a charting library like Chart.js or Recharts here */}
+    </div>
+  );
+};
+
+export default TradingChart;
